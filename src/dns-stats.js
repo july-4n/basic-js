68cae622d32d7904297b953ed171bfec32763135
July-4n <september4n@gmail.com>
2023-12-21 19:50:01 +0500

    feat: solve tasks dns-stats, simple-chain

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const dnsAppearances = {};
+
+  for (let i = 0; i < domains.length; i++) {
+    let str = domains[i].split('.').reverse();
+    let currentDNS = '';
+    for (let j = 0; j < str.length; j++) {
+      currentDNS += `.${str[j]}`;
+      dnsAppearances[currentDNS] = (dnsAppearances[currentDNS] || 0) + 1;
+    }
+  }
+  return dnsAppearances;
 }
 
 module.exports = {
